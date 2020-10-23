c42bf5f215022c172d1e133ee1a492bcfd60bc51
AmimoG <gilbertmatete6@gmail.com>
2020-10-23 14:18:21 +0300

    Add Files

--- istore/views.py
+++ istore/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import datetime
+from .models import * 
 
-# Create your views here.
+def cart(request):
+	data = cartData(request)
+
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/cart.html', context)
